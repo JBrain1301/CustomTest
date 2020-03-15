ea0c6f15163bab15379180c39ea96db5a595a243
Vlad Rusnak <rssvlad@gmail.com>
2020-03-15 21:37:14 +0300

    Самодельный тест

--- src/main/java/org/example/vlad/tests/Testing.java
+++ src/main/java/org/example/vlad/tests/Testing.java
@@ -19,7 +19,7 @@ public class Testing {
 
     @Test
     public void sumTest() {
-        //assertEquals(calc.sum("5 5"," "),11);
+        assertEquals(calc.sum("5 5"," "),11);
         assertEquals(calc.sum("3 5 6", " "), 14);
         assertEquals(calc.sum("5 9 6", " "), 20);
         assertEquals(calc.sum("5 6", " "), 12);
